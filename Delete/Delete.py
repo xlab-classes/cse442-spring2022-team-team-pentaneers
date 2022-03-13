ce3de42018e3023b7cc925eff05ca4f72930abf8
mahdyf45 <56093459+mahdyf45@users.noreply.github.com>
2022-03-13 19:59:21 -0400

    Completed Delete Code #42

---
+++ Delete/Delete.py
@@ -0,0 +1,39 @@
+import Database
+
+
+def deleteSurvey(email, id):
+
+    # Access the Database
+    mydb = Database.dbConnector("root","")
+    mycursor = mydb.cursor()
+
+    # Select only the rows that have our requested "email" 
+    query = "DELETE FROM Surveys WHERE email = %s AND id = %s"
+    values = (email, id)
+    mycursor.execute(query, values)
+    mydb.commit()
+    print("Surveys Table: ", mycursor.rowcount, "record(s) deleted")
+   
+    # Delete from Questions
+    query = "DELETE FROM Questions WHERE survey_id = %s"
+    values = (id,)
+    mycursor.execute(query, values)
+    mydb.commit()
+    print("Questions Table: ", mycursor.rowcount, "record(s) deleted")
+
+    # Delete from Survey_Question table
+    query = "DELETE FROM Survey_Questions WHERE survey_id = %s"
+    values = (id,)
+    mycursor.execute(query, values)
+    mydb.commit()
+    print("Survey_Questions: ", mycursor.rowcount, "record(s) deleted")
+
+    # Delete from Response table
+    query = "DELETE FROM Response WHERE survey_id = %s"
+    values = (id, )
+    mycursor.execute(query, values)
+    mydb.commit()
+    print("Response Table: ", mycursor.rowcount, "record(s) deleted")
+    
+    
+    return ("Survey has been deleted for email: {} with survey_id = {}").format(email, id)
