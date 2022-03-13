090ea38d1f74162ccb34b331d429f64a52f9423b
mahdyf45 <56093459+mahdyf45@users.noreply.github.com>
2022-03-13 19:53:25 -0400

    Connecting to the database #37

---
+++ db_connector.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+
+def dbConnector(usr,pwd):
+    return mysql.connector.connect(
+        host="localhost",
+        user=usr,
+        password=pwd,
+        database = "testdb"
+    )
