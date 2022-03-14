e871b45c5d38c93053598f5d894387a5e2c2d70a
mahdyf45 <56093459+mahdyf45@users.noreply.github.com>
2022-03-14 02:13:42 -0400

    Retrieves Public Surveys (by visibility) #37

--- Retrieve/RetrievePublicSurveys.py
+++ Retrieve/RetrievePublicSurveys.py
@@ -1,4 +1,4 @@
-import Database
+import db_connector
 from application import datetime, date
 
 
@@ -6,7 +6,7 @@ def retrievePublicSurveys():
     List_to_return = []
 
     # Access the Database
-    mydb = Database.dbConnector("root","")
+    mydb = db_connector.dbConnector("root")
     mycursor = mydb.cursor()
 
     # Get all of the surveys from the 'Surveys' table.
@@ -18,15 +18,16 @@ def retrievePublicSurveys():
     # Get todyas date to check whether or not the survey has expired.
     todays_date = date.today()
 
-    # row format = [id, email, title, description, created_on, expire, surveys_id]
+    # row format = [id, email, title, description, created_on, expire, surveys_id, visibility]
     for row in result:
         # This will be the dictionary that we use to add in all Surveys that have not expired
         Dictionary_to_append = {}
         # If the expiration date has not passed, then we add it to a list that we will return.
         expiration_date = row[5]
+        visibility = row[7]
 
-        if expiration_date > todays_date:
-            survey_id = row[6]
+        if expiration_date > todays_date and visibility == 'public':
+            survey_id = row[0]
             survey_title = row[2]
             survey_description = row[3]
             Dictionary_to_append['survey_id'] = survey_id
