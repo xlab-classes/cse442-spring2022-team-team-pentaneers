cb3fa208fe522a7b069b669e366dadebfde8fe9c
mahdyf45 <56093459+mahdyf45@users.noreply.github.com>
2022-03-17 23:16:34 -0400

    Added Error Message if Login/Signup Is Invalid #58

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms.validators import DataRequired, Email, Length, EqualTo, InputRequired
 
 # This contains every field on the Signup page. 
 # It will validate certain user inputs.
@@ -8,11 +8,11 @@ class RegistrationForm(FlaskForm):
     #firstName = StringField('First Name', validators = [DataRequired(), Length(min = 2, max = 30)])
     #lastName = StringField('Last Name', validators = [DataRequired(), Length(min = 2, max = 30)])
 
-    email = StringField('Email', validators = [DataRequired(), Email()])
+    email = StringField('Email', validators = [InputRequired(message="An Email is required!"), DataRequired(), Email(message="Not a valid email")])
 
-    password = PasswordField('Password', validators = [DataRequired(), Length(min = 5, max = 30), EqualTo('confirm_password', message="Passwords must match")])
+    password = PasswordField('Password', validators = [InputRequired(message="A password is required!"), DataRequired(message="Please enter a password"), Length(min = 5, max = 30, message="Password must be between 5 and 30 characters")])
 
-    confirm_password = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password', validators = [InputRequired(message="You must enter a matching password!"), DataRequired(), EqualTo('password', message="Passwords must match")])
 
     submit = SubmitField('Sign Up')
 
