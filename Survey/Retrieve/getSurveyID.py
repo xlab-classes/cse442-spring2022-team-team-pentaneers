4ab1a7e8c594659d3b285a318fe8b0d70173a4fa
mahdyf45 <56093459+mahdyf45@users.noreply.github.com>
2022-04-06 23:43:07 -0400

    Bug Fixes to retrieve specific survey data #64

--- Survey/Retrieve/getSurveyID.py
+++ Survey/Retrieve/getSurveyID.py
@@ -1,11 +1,11 @@
 from db_connector import dbConnector
 
-def surveyID(email, survey_id):
+def surveyID(email, surveys_id):
     # Access the Database
     mydb = dbConnector()
     mycursor = mydb.cursor()
     # Getting the specific survey that belongs to the user
-    query = "SELECT * FROM Surveys WHERE survey_id = %s AND email = %s"
+    query = "SELECT * FROM Surveys WHERE surveys_id = %s AND email = %s"
     value = (survey_id, email)
     mycursor.execute(query, value)
     # Fetch the survey information belonging to the requested Survey
@@ -15,3 +15,19 @@ def surveyID(email, survey_id):
     print("This is the survey: ", survey)
 
     return survey_id
+
+
+def surveysID(email, survey_id):
+    # Access the Database
+    mydb = dbConnector()
+    mycursor = mydb.cursor()
+    # Getting the specific survey that belongs to the user
+    query = "SELECT * FROM Surveys WHERE survey_id = %s AND email = %s"
+    value = (surveys_id, email)
+    mycursor.execute(query, value)
+    # Fetch the survey information belonging to the requested Survey
+    survey = mycursor.fetchall()
+
+    surveys_id = survey[0][6]
+
+    return surveys_id
