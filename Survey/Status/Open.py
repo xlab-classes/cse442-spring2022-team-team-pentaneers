b0a62f136dd709d071681a2b1dd5369baea5ce16
julia <jli379@buffalo.edu>
2022-04-08 22:05:48 -0400

    connect public option to backend #66

--- Survey/Status/Open.py
+++ Survey/Status/Open.py
@@ -4,14 +4,17 @@ from Survey.Retrieve import parseSurveyQuestions, RetrieveSurveyById
 import db_connector
 import datetime
 
+from db_initial import drop
+
+
 def openSurvey(surveys_id,data,email):
     ''' data: {"expires_date": Date or Null}'''
     # when reopen a survey, expires_on may be NULL
     expired = data['expired_date']
-    if(expired==None):
+    if(expired==""):
         mydb = db_connector.dbConnector()
         mycursor = mydb.cursor()
-        close_survey = "UPDATE Surveys SET visibility = %s, expires_on = null WHERE surveys_id = %s and email=%s"
+        close_survey = "UPDATE Surveys SET visibility = %s, expired_on = null WHERE surveys_id = %s and email=%s"
         val = ("public", surveys_id,email)
         mycursor.execute(close_survey, val)
         mydb.commit()
